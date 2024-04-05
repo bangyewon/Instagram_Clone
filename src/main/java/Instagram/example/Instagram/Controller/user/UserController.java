bc17afcb93d2a3a2fe78d21589e5c6da4bb89df5
bangyewon <yewon9272@duksung.ac.kr>
2024-04-06 03:00:07 +0900

    refactor : 로그인 기능 수정

--- src/main/java/Instagram/example/Instagram/Controller/user/UserController.java
+++ src/main/java/Instagram/example/Instagram/Controller/user/UserController.java
@@ -1,15 +1,29 @@
 package Instagram.example.Instagram.Controller.user;
 
-import lombok.RequiredArgsConstructor;
+import Instagram.example.Instagram.web.dto.user.UserLoginDTO;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@RequiredArgsConstructor
 @Controller
 @RequestMapping("/api/v1/member")
 public class UserController {
 
+    @PostMapping("/login")
+    public ResponseEntity<String> login(@RequestBody UserLoginDTO userLoginDTO, HttpServletRequest request) {
+        String username = userLoginDTO.getUsername();
+        String password = userLoginDTO.getPassword();
+        if (username != null && password != null) {
+            HttpSession session = request.getSession();
+            session.setAttribute("username", username);
+            return ResponseEntity.ok("로그인이 성공적으로 됨 ");
+        } else {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("다시 로그인해 주세요");
+        }
+    }
 }
-
-
