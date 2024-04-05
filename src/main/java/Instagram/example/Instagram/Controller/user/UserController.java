9359b103497921a10c7c4858f98138939aae6324
bangyewon <yewon9272@duksung.ac.kr>
2024-04-06 03:38:09 +0900

    refactor : 로그인 기능 수정

--- src/main/java/Instagram/example/Instagram/Controller/user/UserController.java
+++ src/main/java/Instagram/example/Instagram/Controller/user/UserController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/api/v1/member")
 public class UserController {
 
-    @PostMapping("/login")
+    @PostMapping("/login") //로그인 기능
     public ResponseEntity<String> login(@RequestBody UserLoginDTO userLoginDTO, HttpServletRequest request) {
         String username = userLoginDTO.getUsername();
         String password = userLoginDTO.getPassword();
