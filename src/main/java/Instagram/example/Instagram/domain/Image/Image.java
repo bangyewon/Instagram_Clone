0853f14a01cfd86b4caabfe6fe5f5fb69f4545bd
bangyewon <yewon9272@duksung.ac.kr>
2024-04-20 23:06:13 +0900

    bulb image 도메인 주석 추가

--- src/main/java/Instagram/example/Instagram/domain/Image/Image.java
+++ src/main/java/Instagram/example/Instagram/domain/Image/Image.java
@@ -18,9 +18,9 @@ public class Image {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
-    private String caption;
-    private String location;
-    private String imageUrl;
+    private String caption; // 상세설명
+    private String location; //위치
+    private String imageUrl; //사진 전송받아 폴더 저장위함
 
     @ManyToOne(fetch = FetchType.EAGER) // user와 다대일 관계 : 여러 이미지는 하나의 유저에 속하도록
     @JoinColumn(name = "userId") //userId 외래키 - user와 매핑
