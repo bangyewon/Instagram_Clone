a60510ffde16c6a154295749a0166412b9d0f75a
bangyewon <yewon9272@duksung.ac.kr>
2024-04-22 14:44:22 +0900

    refactor ImageController 어노테이션

--- src/main/java/Instagram/example/Instagram/Controller/Image/ImageController.java
+++ src/main/java/Instagram/example/Instagram/Controller/Image/ImageController.java
@@ -7,22 +7,23 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@RestController
+@Controller
+@RequestMapping("/api/v1/images")
 public class ImageController {
+    ImageService imageService;
+    @Autowired
     public ImageController(ImageService imageService) {
         this.imageService = imageService;
     }
-
-    @Autowired
-    ImageService imageService;
     //태그로 이미지 조회
-    @GetMapping("/api/images/ByTag")
-    // "/api/images/ByTags=태그명"으로 이미지 조회 가능
+    @GetMapping("/ByTag")
+    // "/api/v1/images/ByTags=태그명"으로 이미지 조회 가능
     public List<Image> getImagesByTag(@RequestParam String tagName) {
         return imageService.getImagesByTag(tagName);
 
